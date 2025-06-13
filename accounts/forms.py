244b1e0b5f69c168d5751a86df953d621e4c21d9
Laurent Bihin <laurent.bihin@gmail.com>
2025-06-13 19:21:48 +0200

    Le choix concernant le public ou privée des modéles est maintenant enregistré

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from accounts.models import UserSettings
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
@@ -15,3 +17,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+class UserSettingsForm(forms.ModelForm):
+    class Meta:
+        model = UserSettings
+        fields = ("set_public",)
+        widgets = {
+            "set_public": forms.CheckboxInput(
+                attrs={"class": "form-check-input", "id": "public_by_default"}
+            ),
+        }
+        labels = {"set_public": "Rendre les modèles publics par défaut"}
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class UserSettings(models.Model):
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    set_public = models.BooleanField(
+        default=False, verbose_name="Set public by default"
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s settings"
